162d604bebe7441976908660ae00c5e58b5589bc
Aurimas <aurimas@darguzis.com>
2018-07-02 16:53:02 +0100

    covered basics of addNumbers

--- src/libs/add_any_of_the_same_type/__tests__/add_numbers.test.js
+++ src/libs/add_any_of_the_same_type/__tests__/add_numbers.test.js
@@ -4,4 +4,30 @@ describe('Add Numbers test', () => {
     it('has a module', () => {
         expect(add).toBeDefined();
     });
+
+    describe('add', () => {
+        it('adds two positive integers together', () => {
+            const expected = 2;
+            const actual = add.addNumbers(1, 1);
+            expect(actual).toEqual(expected);
+        });
+
+        it('adds two negative integers togeher', () => {
+            const expected = -2;
+            const actual = add.addNumbers(-1, -1);
+            expect(actual).toEqual(expected);
+        });
+
+        it('adds a negative and a positive integers togehter', () => {
+            const expected = 0;
+            const actual = add.addNumbers(1, -1);
+            expect(actual).toEqual(expected);
+        });
+
+        it('adds zero to zero', () => {
+            const expected = 0;
+            const actual = add.addNumbers(0, 0);
+            expect(actual).toEqual(expected);
+        });
+    });
 });
