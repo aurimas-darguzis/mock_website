8551cc6adbf0402eed8af211a7d7b71ca08d792b
Aurimas <aurimas@darguzis.com>
2018-07-03 16:29:33 +0100

    test covers Numbers

--- src/libs/add_any_of_the_same_type/__tests__/add_numbers.test.js
+++ src/libs/add_any_of_the_same_type/__tests__/add_numbers.test.js
@@ -29,5 +29,61 @@ describe('Add Numbers test', () => {
             const actual = add.addNumbers(0, 0);
             expect(actual).toEqual(expected);
         });
+
+        it('returns -1 for undefined input', () => {
+            const expected = -1;
+            const actual = add.addNumbers();
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 if the left parameter is undefined', () => {
+            const expected = -1;
+            const actual = add.addNumbers(undefined, 1);
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 if the right parameter is undefined', () => {
+            const expected = -1;
+            const actual = add.addNumbers(1, undefined);
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 for adding a number to a string', () => {
+            const expected = -1;
+            const actual = add.addNumbers(1, 'undefined');
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 for adding a number to a boolean', () => {
+            const expected = -1;
+            const actual = add.addNumbers(1, true);
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 for adding a number to an array', () => {
+            const expected = -1;
+            const actual = add.addNumbers(1, []);
+            expect(actual).toEqual(expected);
+        });
+
+        it('returns -1 for adding a number to an object', () => {
+            const expected = -1;
+            const actual = add.addNumbers(1, {});
+            expect(actual).toEqual(expected);
+        });
+    });
+
+    describe('isFailedNumberAdd', () => {
+        it('returns true if number is -1', () => {
+            const expeceted = true;
+            const actual = add.isFailedNumberAdd(-1);
+            expect(actual).toEqual(expeceted);
+        });
+
+        it('returns false if number is not -1', () => {
+            const expeceted = false;
+            const actual = add.isFailedNumberAdd(1);
+            expect(actual).toEqual(expeceted);
+        })
     });
 });
--- src/libs/add_any_of_the_same_type/_add_numbers.js
+++ src/libs/add_any_of_the_same_type/_add_numbers.js
@@ -1,5 +1,12 @@
 const addNumbers = (a, b) => {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') return -1;
     return a + b;
 }
 
 module.exports.addNumbers = addNumbers;
+
+const isFailedNumberAdd = value => {
+    return value === -1;
+}
+
+module.exports.isFailedNumberAdd = isFailedNumberAdd;
