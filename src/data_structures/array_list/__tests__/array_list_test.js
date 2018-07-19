1df15cfad4304f39b8cb7f4c57ec8f18e52544b5
Aurimas <aurimas@darguzis.com>
2018-07-19 12:55:02 +0100

    array list implementation

---
+++ src/data_structures/array_list/__tests__/array_list_test.js
@@ -0,0 +1,11 @@
+const arrayList = require('../arrayList');
+
+describe('ArrayList', () => {
+    it('should be defined', () => {
+        expect(arrayList).toBeDefined();
+    });
+
+    it('should do stuff', () => {
+        
+    })
+});
