7a6f35681af2343ac66f1af5565c507bc4998421
Aurimas <aurimas@darguzis.com>
2018-07-22 15:21:38 +0100

    added linkedList

---
+++ src/data_structures/linked_list/__tests__/linked_list_test.js
@@ -0,0 +1,11 @@
+const linkedList = require('../linkedList');
+
+describe('linkedList', () => {
+    it('should be defined', () => {
+        expect(arrayList).toBeDefined();
+    });
+
+    it('should do stuff', () => {
+        
+    })
+});
